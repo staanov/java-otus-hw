3f4ef1eafa2931f0115f1ced066af96fbac658d4
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2024-01-19 18:49:18 +0300

    hw-02 initial state

---
+++ hw02-generics/build.gradle.kts
@@ -0,0 +1,15 @@
+tasks.bootJar {
+    enabled = false
+}
+
+dependencies {
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "java-otus-hw"
 include("hw01-gradle")
+include("hw02-generics")
