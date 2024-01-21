dc4a83b87bb913e9976c806cc2d65e91986d4f68
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2024-01-21 18:34:12 +0300

    hw-02 add solution

--- hw02-generics/src/main/java/homework/Customer.java
+++ hw02-generics/src/main/java/homework/Customer.java
@@ -45,16 +45,11 @@ public class Customer {
 
         Customer customer = (Customer) o;
 
-        if (id != customer.id) return false;
-        if (scores != customer.scores) return false;
-        return name != null ? name.equals(customer.name) : customer.name == null;
+        return id == customer.id;
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (int) (scores ^ (scores >>> 32));
-        return result;
+        return (int) (id ^ (id >>> 32));
     }
 }
--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,13 +1,18 @@
 package homework;
 
+import java.util.ArrayDeque;
+
 public class CustomerReverseOrder {
 
     // todo: 2. надо реализовать методы этого класса
     // надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    ArrayDeque<Customer> deque = new ArrayDeque<>();
 
-    public void add(Customer customer) {}
+    public void add(Customer customer) {
+        deque.add(customer);
+    }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return deque.pollLast();
     }
 }
