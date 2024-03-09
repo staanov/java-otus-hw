a6a8e2f7a779bfa8fbf11cc3aa2cd63ddc18092b
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2024-03-09 18:55:20 +0300

    hw-08 initial state

---
+++ hw08-serialization/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.bootJar {
+    enabled = false
+}
