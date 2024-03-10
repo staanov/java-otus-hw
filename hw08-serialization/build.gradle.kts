ce87e667b59b10588ecaa218d1adb57d3e6543bc
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2024-03-10 13:38:36 +0300

    hw-08 add solution

--- hw08-serialization/build.gradle.kts
+++ hw08-serialization/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core")
+    implementation("com.google.code.gson:gson")
 }
 
 tasks.test {
--- hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,17 +1,33 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import ru.otus.model.Measurement;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class ResourcesFileLoader implements Loader {
 
-    public ResourcesFileLoader(String fileName) {}
+    private final String path;
+    private final Gson gson = new Gson();
+
+    public ResourcesFileLoader(String fileName) {
+        this.path = fileName;
+    }
 
     @Override
     public List<Measurement> load() {
         // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+      try (var reader = new InputStreamReader(Objects.requireNonNull(ClassLoader.getSystemResourceAsStream(path)))) {
+        var type = TypeToken.getParameterized(List.class, Measurement.class).getType();
+        return gson.fromJson(reader, type);
+      } catch (IOException e) {
+          throw new FileProcessException(e);
+      }
     }
 }
